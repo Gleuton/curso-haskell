eb57b3afc31eda932b368b5056a84830fdc84a3f
Gleuton <gleuton.dutra@gmail.com>
2017-08-02 14:14:17 -0300

    funcao para ordenar lista crescentemente  _ ordena parte 2

--- sort.hs
+++ sort.hs
@@ -1,5 +1,5 @@
 list :: [Int]
-list = [5,2,10,8,1]
+list = [5,2,10,8,1,1,11]
 
 get_smaller :: [Int] -> Int
 get_smaller [x] = x
@@ -10,3 +10,11 @@ remove_smaller :: [Int] -> [Int]
 remove_smaller [] = []
 remove_smaller (x:xs) | (x == (get_smaller (x:xs))) = xs
                       | otherwise = (x:remove_smaller xs)
+
+aux_sort :: [Int] -> [Int] -> [Int]
+aux_sort ordered_list [] = ordered_list
+aux_sort ordered_list (x:xs) = aux_sort (ordered_list++[get_smaller (x:xs)]) (remove_smaller (x:xs))
+
+sort :: [Int] -> [Int]
+sort [] = []
+sort list = aux_sort [] list
