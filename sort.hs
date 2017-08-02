254496de6c72bc7431a98b0b9d0f1ab6f0d190a1
Gleuton <gleuton.dutra@gmail.com>
2017-08-02 11:31:02 -0300

    funcao para remover o menor numenor da lista _ ordena parte 1.5

--- sort.hs
+++ sort.hs
@@ -1,7 +1,12 @@
 list :: [Int]
 list = [5,2,10,8,1]
 
-get_small :: [Int] -> Int
-get_small [x] = x
-get_small (x:xs) | (x < get_small xs) = x
-                 | otherwise = get_small xs
+get_smaller :: [Int] -> Int
+get_smaller [x] = x
+get_smaller (x:xs) | (x < get_smaller xs) = x
+                 | otherwise = get_smaller xs
+
+remove_smaller :: [Int] -> [Int]
+remove_smaller [] = []
+remove_smaller (x:xs) | (x == (get_smaller (x:xs))) = xs
+                      | otherwise = (x:remove_smaller xs)
